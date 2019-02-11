44bb492d1a77a5ed7a9c5267426275b236f27348
Mykenzie Rogers <mykenzierogers@Mykenzies-MacBook-Pro.local>
2019-02-11 10:36:30 -0600

    merge firebase

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <>
-      </>
-=======
       <div className="App">
     
       </div>
->>>>>>> master
     );
   }
 }
