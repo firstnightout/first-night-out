c3815d96a117ca2dc03d9c9b1b13e7043ae8760d
unknown <jeremylscott@gmail.com>
2019-02-11 14:42:03 -0600

    Styled the register mobile view and refactored the methods from inline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+      <Register/>
       </div>
     );
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,50 @@
 import React, {useState} from 'react'
+import logo from '../../images/fno.png'
 // import {connect} from 'react-redux'
 // import {Link} from 'react-router-dom'
 // import {handleLogin} from '../../ducks/reducer'
 import './login.css'
 
+
 const UserLogin = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    
+    const handleUpdates = (e) => {
+        if(username) {
+            setUsername(e.target.value)
+        }
+        else if(password) {
+            setPassword(e.target.value)
+        }
+    }
+
+    // const handleLogin = (username,password) => {
+
+    // }
 
     return (
         <div className='loginCont'>
-            <img src='' className='login-logo' alt='logo'/>
-            <input className='username-login' onChange={(e) => setUsername(e.target.value)} placeholder='Username'
-            />
-            <input className='pass-login' onChange={(e) => setPassword(e.target.value)} placeholder='Password'
-            />
-            {/* <div>
-                <button className='login-button onClick={() => props.handleLogin(username,password)}>Login</button>
-                <Link to='/register'><button className='register-button'>Register</button></Link>
-            </div> */}
+            <div className='imageCont'>
+                <img src={logo} className='login-logo' alt='logo'/>
+            </div>
+            <div className='inputCont'>
+                <input className='username-login' onChange={handleUpdates} placeholder='Username'
+                />
+                <input className='pass-login' onChange={handleUpdates} placeholder='Password'
+                />
+            </div>
+            <div className='buttonCont'>
+                    <button className='login-button'> {/*onClick={this.handleLogin(username,password)}*/}Login</button>
+                    <button className='register-button'>Register</button>
+                {/* <Link to='/register'></Link> */}
+            </div>
+            <div className='buttonCont2'>
+                <button className='passportButton'>Login with Passport</button>
+                <button className='googleButton'>Login with Google</button>
 
+            </div>
+            
             {username ? <span className='username-label'>Username</span>
             :
             null
@@ -33,9 +58,8 @@ const UserLogin = () => {
     )
 }
 
-// const mapStateToProps = state => state
+{/* const mapStateToProps = state => state */}
 
 export default UserLogin
 
-
-// export default connect(mapStateToProps, {handleLogin})(UserLogin)
+{/* export default connect(mapStateToProps, {handleLogin})(Login) */}
