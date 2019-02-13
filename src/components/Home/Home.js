a53e7287b6af7242d6acfb253862a427a038a387
Mykenzie Rogers <mykenzierogers@Mykenzies-MacBook-Pro.local>
2019-02-13 15:21:40 -0600

    fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import MiniRoute from './../MiniRoute/MiniRoute'
+import MiniPlace from './../MiniPlace/MiniPlace'
+import MiniRoute from '../MiniRoute/MiniRoute'
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import './Home.css'
 
 const Home = (props) => {
     //REMEMBER TO HIDE API KEY LATER
@@ -13,13 +17,49 @@ const Home = (props) => {
         axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${temp}&key=AIzaSyA0dEOfis7q8Pl8_MM5uhen6ustyIGwCvQ`).then(async response => {
             let {lat, lng} = response.data.results[0].geometry.location
             const location = `${lat},${lng}`;
-            axios.post('/api/places/near', {location, radius: 500}).then(response2 => {
-                setNearbyPlaces(response2.data.results);
+            axios.post('/api/places/near', {location, radius: 500, type: 'restaurant'}).then(response2 => {
+                console.log(response2.data.results);
+                let renderArr = response2.data.results.map((val, i, arr) => {
+                    console.log(val.photos);
+                    if(val.photos) {
+                        return <MiniPlace place_id={val.place_id} photo={val.photos[0].photo_reference} />
+                    }
+                    else {
+                        return <MiniPlace place_id={val.place_id} />
+                    }
+                    //
+                })
+                setNearbyPlaces(renderArr);
             });
         })
     }, []);
     return(
-        <div>
+        <div className='home'>
+            <Carousel
+            animationSpeed={2000}
+            autoPlay={5000}
+            stopAutoPlayOnHover
+            centered
+            infinite>
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/restaurant-picture-1.jpg?alt=media&token=140b8892-a6e0-4b55-9b79-ac1585e98cb9`} />
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/bar-1.jpg?alt=media&token=337920c8-9353-4914-b56d-b8259c5e3020`} />
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/zoo-1.jpg?alt=media&token=af6666a2-a219-4ae2-9da2-9225200f0d48`} />
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/restaurant-2.jpg?alt=media&token=d12a1d52-1a26-4a82-b988-8ca54d90e9b4`} />
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/bar-2.jpg?alt=media&token=bc638ff9-9f76-4158-b303-6e13411da981`} />
+                <img className='carousel-img' src={`https://firebasestorage.googleapis.com/v0/b/first-night-out.appspot.com/o/spa-1.jpg?alt=media&token=95c951ea-b4c1-403f-abbd-fe2d469d341d`} />
+            </Carousel>
+            {/* {nearbyPlaces} */}
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
+            <MiniRoute />
             <MiniRoute />
             <MiniRoute />
             <MiniRoute />
--- src/components/MiniPlace/MiniPlace.js
+++ src/components/MiniPlace/MiniPlace.js
@@ -4,71 +4,26 @@ import './MiniPlace.css'
 
 const MiniPlace = (props) => {
     const [ref, setRef] = useState(0);
-    const testPlace = {
-            "geometry": {
-                "location": {
-                    "lat": 32.7811852,
-                    "lng": -96.795757
-                },
-                "viewport": {
-                    "northeast": {
-                        "lat": 32.7826196302915,
-                        "lng": -96.7944340697085
-                    },
-                    "southwest": {
-                        "lat": 32.7799216697085,
-                        "lng": -96.79713203029151
-                    }
-                }
-            },
-            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
-            "id": "99768f3e883fc4dbb573609ed75ecaceeada5639",
-            "name": "Wild Salsa",
-            "opening_hours": {
-                "open_now": true
-            },
-            "photos": [
-                {
-                    "height": 1440,
-                    "html_attributions": [
-                        "<a href=\"https://maps.google.com/maps/contrib/112887384560088893539/photos\">Raul S.</a>"
-                    ],
-                    "photo_reference": "CmRaAAAAv9DGDMaMB9BPbuJBGXgtD3eipfCItP6gN6fML32G8umC9TM9nrsnXdX9tnAO-2lBbpNH-0Ye6yhYtxYY_rgMGd00ZLqSA4kHGPmVhXyLyn9J-aHb_cKNX7GenusvzM7kEhCGk2PK1eMQz7Zu6QxHFGXrGhTzK3OpwUs8FGmTow2nizJdAR_9Mg",
-                    "width": 2560
-                }
-            ],
-            "place_id": "ChIJFyXWQx-ZToYR9qFvdYlDjtA",
-            "plus_code": {
-                "compound_code": "Q6J3+FM Dallas, Texas, United States",
-                "global_code": "8645Q6J3+FM"
-            },
-            "price_level": 2,
-            "rating": 4.1,
-            "reference": "ChIJFyXWQx-ZToYR9qFvdYlDjtA",
-            "scope": "GOOGLE",
-            "types": [
-                "bar",
-                "restaurant",
-                "point_of_interest",
-                "food",
-                "establishment"
-            ],
-            "user_ratings_total": 649,
-            "vicinity": "1800 Main Street, Dallas"
-        }
-        useEffect(() => {
-            axios.post(`/api/places/details`, {
-                placeid: testPlace.place_id,
-            }).then(response => {
-                setRef(response.data.result);
-                console.log('response.data.result: ', response.data.result);
-            })
-        }, [])
+    const [name, setName] = useState(0);
+
+    useEffect(() => {
+        axios.post(`/api/places/details`, {
+            placeid: props.place_id,
+        }).then(response => {
+            setRef(response.data.result);
+            if(response.data.result.name.length > 30) {
+                setName(response.data.result.name.substring(0,27) + '...');
+            } else {
+                setName(response.data.result.name);
+            }
+        })
+    }, [])
+    
     return(
         <div className='mini-place'>
-            <img className='mini-place-photo' src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${testPlace.photos[0].photo_reference}&maxheight=100&key=AIzaSyB3hkAtDj8ZZK9ptagSp_YqQouPEMcuaCo`} />
+            <img className='mini-place-photo' src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${props.photo}&maxheight=100&key=AIzaSyB3hkAtDj8ZZK9ptagSp_YqQouPEMcuaCo`} />
             <div className='mini-place-info'>
-                <h1>{testPlace.name}</h1>
+                <h1>{name}</h1>
                 <h3>{ref.formatted_address ? ref.formatted_address.substring(0, ref.formatted_address.indexOf(',', ref.formatted_address.indexOf(',') + 1)) : null }</h3>
                 <h3>{ref.rating}</h3>
             </div>
