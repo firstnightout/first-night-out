a5bae84419bb07d0593a5b49c06d2f4a50adf073
unknown <jeremylscott@gmail.com>
2019-02-13 10:27:28 -0600

    Created nav component and added ref to index.html

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,20 @@ import {Link} from 'react-router-dom'
 // import {} from '../../ducks/reducer'
 import './nav.css'
 
-function Nav() {}
+const Nav = () => {
+
+    return (
+        <div>
+            <div>
+                <i className="fas fa-bars"></i>
+                <span>{props.users[0].username}</span>
+            </div>
+            <div>
+                <img src={props.users[0].profilePic} className='navUserImg'/>
+            </div>
+        </div>
+    )
+}
 
 
 const mapStateToProps = state => state
