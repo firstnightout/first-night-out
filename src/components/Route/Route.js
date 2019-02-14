3b442e6bbe332d0ac783b3f63044faeca118a6e9
unknown <jeremylscott@gmail.com>
2019-02-14 09:41:52 -0600

    Changed up the Route component since we are going to be reusing the mini map code to show these views

--- src/components/Route/Route.js
+++ src/components/Route/Route.js
@@ -4,27 +4,12 @@ import {Link} from 'react-router-dom'
 
 const Route = (props) => {
 
-    const route = props.routes.map(route => {
-        return (
-            <div className='routeWrapper'>
-                <div>
-                    <img src={route.img} key={route.id}/>
-                </div>
-                <div>
-                    <span>{route.venueName}</span>
-                    <span>{route.venueAddress}</span>
-                    <span>{route.rating}</span>
-                </div>
-            </div>
-        )
-    })
-
     return(
-        <div>
+        <div className='routeMain'>
             <div className='routeScreen'>
                 <p>My Route</p>
                 <Link to='/map' className='mapRoute'>GO</Link>
-                {route}
+                
             </div>
         </div>
     )
