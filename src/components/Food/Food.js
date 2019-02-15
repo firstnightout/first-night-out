286159824c4f1bc832505875aaa5299187feddd3
unknown <jeremylscott@gmail.com>
2019-02-15 12:30:59 -0600

    Added drop downs and connected food component

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -7,9 +7,13 @@ import './food.css'
 const Food = (props) => {
     const[selection, setSelection] = useState()
 
+    const handleChange = (e) => {
+        setSelection(e.target.value);
+    }
+
     return (
-        <div>
-            <select className='foodDropDown' name='selected'>
+        <div className='foodWrapper'>
+            <select className='foodDropDown' name='selected' onChange={handleChange}>
                 <option value='empty'></option>
                 <option value='bakery'>Bakery</option>
                 <option value='cafe'>Cafe</option>
