deba6c87c1a6e0adc0412d853d714bde01f4c7c4
unknown <jeremylscott@gmail.com>
2019-02-26 09:37:09 -0600

    Commit after pulling down and npm i

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,10 +1,5 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
-<<<<<<< HEAD
-=======
-// import * as admin from'firebase-admin'
-
->>>>>>> master
 
 let hidden = process.env;
 var config = {
